403b92738631b62b7f87d98b5801b47567caafc0
samy670 <samy670@WINDOWS>
2016-07-07 15:31:40 +0200

    Add project files.

---
+++ TonyW_Expanders_Example/MainFormViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TonyW_Expanders_Example
+{
+    public class MainFormViewModel : BindableObject
+    {
+        public ObservableCollection<SimpleModel> AvailableColumns { get; set; }
+        public ObservableCollection<SimpleModel> VisibleColumns { get; set; }
+
+        public SimpleModel CurrentItemLeft
+        {
+            get
+            {
+                return _currentItemLeft;
+            }
+            set
+            {
+                _currentItemLeft = value;
+                RaisePropertyChanged("CurrentItemLeft");
+            }
+        }
+        public SimpleModel CurrentItemRight
+        {
+            get
+            {
+                return _currentItemRight;
+            }
+            set
+            {
+                _currentItemRight = value;
+                RaisePropertyChanged("CurrentItemRight");
+            }
+        }
+
+        public ICommand ShiftToRightCommand { get; private set; }
+        public ICommand ShiftToLeftCommand { get; private set; }
+
+        public MainFormViewModel()
+        {
+            AvailableColumns = new ObservableCollection<SimpleModel>();
+            VisibleColumns = new ObservableCollection<SimpleModel>();
+
+            ShiftToRightCommand = new RelayCommand(ExecuteShiftToRight, CanExecuteShiftToRight);
+            ShiftToLeftCommand = new RelayCommand(ExecuteShiftToLeft, CanExecuteShiftToLeft);
+            Load();
+        }
+
+        public void Load()
+        {
+            //normall you would
+            //load your data from a db or webservice
+
+            //Some dummy Data
+            AvailableColumns.Add(new SimpleModel() { FirstName = "Louise", LastName="Burling" });
+            AvailableColumns.Add(new SimpleModel() { FirstName = "Hana", LastName = "Gregor" });
+            AvailableColumns.Add(new SimpleModel() { FirstName = "Don", LastName = "Giovanni" });
+        }
+
+        #region Commands
+        private bool CanExecuteShiftToRight(object obj)
+        {
+            return CurrentItemLeft != null;
+        }
+        private void ExecuteShiftToRight(object obj)
+        {
+            var temp = CurrentItemLeft;
+            CurrentItemLeft = null;
+
+            AvailableColumns.Remove(temp);
+            VisibleColumns.Add(temp);
+
+            CurrentItemLeft = null;
+            CurrentItemRight = null;
+            CurrentItemRight = VisibleColumns.LastOrDefault();
+        }
+
+
+        private bool CanExecuteShiftToLeft(object obj)
+        {
+            return CurrentItemRight != null;
+        }
+        private void ExecuteShiftToLeft(object obj)
+        {
+            var temp = CurrentItemRight;
+            CurrentItemRight = null;
+
+            VisibleColumns.Remove(temp);
+            AvailableColumns.Add(temp);
+
+            CurrentItemLeft = null;
+            CurrentItemRight = null;
+            CurrentItemLeft = AvailableColumns.LastOrDefault();
+        }
+        #endregion
+
+
+        #region Private/Protected
+        private SimpleModel _currentItemLeft;
+        private SimpleModel _currentItemRight;
+        #endregion
+    }
+}
---
+++ TonyW_Expanders_Example/SimpleModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TonyW_Expanders_Example
+{
+    public class SimpleModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return (LastName == null && FirstName == null) ? "You haven't specified an Name" : String.Format("{0}, {1}", LastName, FirstName);
+        }
+    }
+}
